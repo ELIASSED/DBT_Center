639f24459d37fd37e3cfaac4745e9e22cadf7910
Elias Seddiki <eliasseddiki@MacBook-Pro-de-Elias.local>
2024-06-26 19:32:12 +0200

    Updates

--- src/components/index.js
+++
@@ -1,7 +0,0 @@
-export { default as Navbar } from "./Navbar/Navbar";
-export { default as Footer } from "./Footer/Footer";
-export { default as InfoSection } from "./InfoSection/InfoSection";
-export { default as TherapySection } from "./TherapySection/TherapySection";
-export { default as TeamSection } from "./TeamSection/TeamSection";
-export { default as ServicesSection } from "./ServicesSection/ServicesSection";
-export { default as ModuleSection } from "./ModuleSection/ModuleSection";
